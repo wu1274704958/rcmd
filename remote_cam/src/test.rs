2c1d64b7a2d5c94166d77abadb0800de34da7730
wws <1274704958@qq.com>
2022-12-16 20:47:58 +0800

    l.

--- remote_cam/src/test.rs
+++ remote_cam/src/test.rs
@@ -4,11 +4,15 @@ mod client;
 mod model;
 mod client_plugs;
 
+use std::{sync::Arc, thread::sleep, time::Duration};
+
 use client::scl::run;
-use tokio::runtime::Runtime;
+use tokio::{runtime::Runtime, sync::Mutex};
 
 fn main() {
     let runtime = Runtime::new().unwrap();
-    runtime.spawn(run());
+    let str = "-i 127.0.0.1  -b 8081 -p 8080".to_string();
+    runtime.spawn(run(str,Arc::new(Mutex::new(true))));
+    sleep(Duration::from_secs(1000*1000));
     println!("---");
 }
